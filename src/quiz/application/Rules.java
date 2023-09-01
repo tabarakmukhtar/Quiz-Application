da30dab843fecf3dce04215f250dbea95e01019a
TABARAK MUKHTAR <107870133+tabarakmukhtar@users.noreply.github.com>
2023-09-01 19:29:51 +0530

    Initial Commit

---
+++ src/quiz/application/Rules.java
@@ -0,0 +1,72 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Quiz Challenge");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. Focus on providing concise and accurate answers as a programmer." + "<br><br>" +
+                "2. Maintain a respectful atmosphere; avoid unnecessary distractions." + "<br><br>" +
+                "3. Answer all questions as they are mandatory." + "<br><br>" +
+                "4. If needed, express your emotions quietly." + "<br><br>" +
+                "5. Prioritize providing wise answers over asking questions." + "<br><br>" +
+                "6. Don't be concerned if others are answering more questions." + "<br><br>" +
+                "7. Prepare for challenging questions; this quiz is meant to be challenging." + "<br><br>" +
+                "8. Wishing you the best of luck in your quiz endeavors." + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
